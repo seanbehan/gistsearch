6f40e1f0fc6ef790653f2a8e594effd66a0d7827
Sean Behan <inbox@seanbehan.com>
2016-07-28 17:34:59 -0400

    m

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template as render, request, redirect, jsonify
+from flask import Flask, render_template as render, request, redirect, jsonify, session
 from os import environ as env, path
 from requests import post
 from urllib import urlencode, unquote, quote
@@ -9,6 +9,23 @@ from worker import conn
 
 q = Queue(connection=conn)
 app = Flask(__name__)
+app.secret_key = '\x9aT\xa5rm\xeah\xe6\xf3\xdb\x11\x00\x8f\x16\x084\xc1\x0e\x13\x0bYM\x11R'
+
+@app.before_request
+def before_request():
+    if request.path in ('/login', '/authorize', '/callback'):
+        return None
+    if not session.get('access_token'):
+        return redirect('/login')
+
+@app.route("/login")
+def login():
+    return render('login.html')
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    session.pop('access_token')
+    return redirect('/login')
 
 @app.route("/")
 def home():
@@ -21,9 +38,9 @@ def search():
     gists = [gist for gist in db.query('select * from gists where description ~* :q', q=q)]
     return render('search.html', gists=gists)
 
-@app.route("/language")
-def language():
-    q = request.args.get('lgn')
+@app.route("/language/<language>")
+def language(language):
+    q = str(language).lower()
     gists = [gist for gist in db.query('select distinct (f.gist_id, f.filename), g.*,f.* from gists g join files f on g.gist_id = f.gist_id where f.language = :q order by f.gist_id,f.filename', q=q)]
 
     return render('search.html', gists=gists)
@@ -33,7 +50,6 @@ def file_contents():
     q = unquote(request.args.get('txt'))
     gists = [gist for gist in db.query('select distinct (g.gist_id), * from gists g, files f where g.gist_id = f.gist_id and f.content ilike :q', q="%"+q+"%")]
     return render('search.html', gists=gists)
-
 
 @app.route("/authorize")
 def authorize():
@@ -46,9 +62,10 @@ def callback():
     resp = post("https://github.com/login/oauth/access_token?%s" % query_string).content
 
     access_token = parse_qs(resp)['access_token'][0]
-    db['users'].upsert(dict(id=1, access_token=access_token), ['id'])
+    db['users'].upsert(dict(access_token=access_token), ['access_token'])
 
     result = q.enqueue(save_gists, access_token)
+    session['access_token'] = access_token
 
     return redirect('/')
 
